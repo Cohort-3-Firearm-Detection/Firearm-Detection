e0e21b863f6fe9b5d249fea33984189aa89c9f8f
abcovarrubio <104332011+abcovarrubio@users.noreply.github.com>
2022-08-17 10:06:31 -0500

    simplified trigger without need to test.py file

--- trigger.py
+++ trigger.py
@@ -4,9 +4,7 @@ import subprocess
 def detection(object):
     if object == True:
         subprocess.call(['python', 'alert.py'])
-    else:
-        subprocess.call(['python', 'test.py'])
-    return 'Need help'
+    print( 'Need help')
 
 
 if __name__ == '__main__':
