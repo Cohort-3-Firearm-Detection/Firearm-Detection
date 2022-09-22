b4852ec60bc720a57a978473f1cd85889886b438
Covarrubio <acovarrubio@teksystems.com>
2022-09-22 14:56:22 -0500

    send_email needs work

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # from send_mms_alert.send_mms import SendText
 from send_email_alert.send_email import SendEmail
 
+
 # PATH = "Tensorflow/workspace/images/guntest1.png"
 
 # SendText.image_upload(PATH)
 # SendText.mms(number = '+15022165263')
 
-SendEmail.email(cls = SendEmail, recipients= 'acovarrubio@teksystems.com')
+SendEmail.email()
 # SendEmail.recipients = 'jerewalker@teksystems.com', 'mdave@teksystems.com', 'jhagerman@teksystems.com', 'acovarrubio@teksystems.com', 'dchamness@teksystems.com'
--- send_email_alert/send_email.py
+++ send_email_alert/send_email.py
@@ -1,32 +1,124 @@
-from flask import Flask
-from flask_mail import Mail, Message
+#### SendGrid Email###
+import sendgrid
+import os
+from sendgrid.helpers.mail import Mail, Email, To, Content, Attachment, FileContent, FileType, FileName, Disposition, HtmlContent
+import base64
 
 class SendEmail:
-    app = Flask(__name__)
-    mail = Mail(app)
-    app.config['MAIL_SERVER']='smtp.office365.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USERNAME'] = 'fva_alert@outlook.com'
-    app.config['MAIL_PASSWORD'] = 'FVA@22ml'
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USE_SSL'] = False
-    app.config['MAIL_ASCII_ATTACHMENTS'] = False
+    @classmethod
+    def email(self):
+        sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+        from_email = Email("fva_alert@outlook.com")  # Change to your verified sender
+        to_email = [To('jerewalker@teksystems.com'), To('acovarrubio@teksystems.com')]  # Change to your recipient
+        subject = "Detection Found"
+         
+        content = Content("text/plain", "A detection has been made. See image.")
+        html_content = HtmlContent('<html><body><img class "fit-picture" src="../Tensorflow/workspace/images/guntest1.png"/></body></html>')
+        
+        mail = Mail(from_email, to_email, subject, content, html_content)
+
+        # Get a JSON-ready representation of the Mail object
+        mail_json = mail.get()
+
+        # Send an HTTP POST request to /mail/send
+        response = sg.client.mail.send.post(request_body=mail_json)
+        print(response.status_code)
+        print(response.headers)
 
 
-    @app.route("/")
-    def email(cls, recipients):
-        msg = Message(
-				    'Hello',
-				    sender ='fva_alert@outlook.com',
-				    recipients = [recipients] 
-			    )
-        msg.body = 'Hello a threat was detected with a confidence level of. See attached image. Flask message sent from Flask-Mail. Do not respond to this message.'
-        with cls.app.open_resource('../Tensorflow/workspace/images/guntest1.png') as detect:
-            msg.attach('detection.png', 'image/png', detect.read())
-        cls.mail.send(msg)
-        return('Sent')
-    if __name__ == '__main__':
-        app.run(debug = True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from flask import Flask
+# from flask_mail import Mail, Message
+
+
+
+# app = Flask(__name__)
+# app.config.update(
+#     DEBUG = True,
+#     MAIL_SERVER='smtp.office365.com',
+#     MAIL_PORT = 587,
+#     MAIL_USERNAME = 'abcovarrubio@outlook.com',
+#     MAIL_PASSWORD = 'OUTLOOK!T4nC4yd',
+#     MAIL_USE_TLS = True,
+#     MAIL_USE_SSL = False,
+#     MAIL_ASCII_ATTACHMENTS = False
+#     )
+# mail = Mail(app)
+
+
+# ######## Live stuff ############
+# @app.route('/')
+# def email(recipients):
+#     try:
+#         msg = Message('Hello',
+# 			sender ='abcovarrubio@outlook.com',
+# 			recipients = [recipients] )
+#         msg.body = 'Hello a threat was detected with a confidence level of. See attached image. Flask message sent from Flask-Mail. Do not respond to this message.'
+#         # with cls.app.open_resource('../Tensorflow/workspace/images/guntest1.png') as detect:
+#         #     msg.attach('detection.png', 'image/png', detect.read())
+#         mail.send(msg)
+#         return "Sent"
+    
+#     except Exception as e:
+#         return str(e)
+
+
+
+
+
+
+
+
+
+
+
+# class SendEmail:
+#     @classmethod
+#     @app.route("/")
+#         def email(cls, recipients):
+
+    
+#         msg = Message(
+# 		    	'Hello',
+# 			        sender ='abcovarrubio@outlook.com',
+# 				    recipients = [recipients] 
+# 			     )
+#         msg.body = 'Hello a threat was detected with a confidence level of. See attached image. Flask message sent from Flask-Mail. Do not respond to this message.'
+#         # with cls.app.open_resource('../Tensorflow/workspace/images/guntest1.png') as detect:
+#         #     msg.attach('detection.png', 'image/png', detect.read())
+#         mail.send(msg)
+#         return "Sent"
+
+
 
 
 # class SendEmail:
@@ -37,8 +129,8 @@ class SendEmail:
 #         mail = Mail(app)
 #         app.config['MAIL_SERVER']='smtp.office365.com'
 #         app.config['MAIL_PORT'] = 587
-#         app.config['MAIL_USERNAME'] = 'abcovarrubio@outlook.com'
-#         app.config['MAIL_PASSWORD'] = 'OUTLOOK!T4nC4yd'
+#         app.config['MAIL_USERNAME'] = 'fva_alert@outlook.com'
+#         app.config['MAIL_PASSWORD'] = 'FVA@22ml'
 #         app.config['MAIL_USE_TLS'] = True
 #         app.config['MAIL_USE_SSL'] = False
 #         app.config['MAIL_ASCII_ATTACHMENTS'] = False
