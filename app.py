10fcbb3e4936465589833307a877030be3530c82
Covarrubio <acovarrubio@teksystems.com>
2022-09-22 18:55:41 -0500

    Add MSG variable for text and email body to include confidence level

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-# from send_mms_alert.send_mms import SendText
-# from send_email_alert.send_email import SendEmail
+from send_mms_alert.send_mms import SendText
+# # from send_email_alert.send_email import SendEmail
 from Tensorflow.live_detect_app import *
 
 
 PATH = "Tensorflow/workspace/images/detection.png"
+MSG = "A detection has been made with a confidence level of {}. See the attached image for more information".format(Detection.conf_lev)
+
 
 Detection.live_detect()
 if Detection.conf_lev >= 0.7:
     print('Detection made with a confidence level of {}'.format(Detection.conf_lev))
-    # SendText.image_upload(PATH)
-    # SendText.mms('+12146051373')
+    SendText.image_upload(PATH)
+    SendText.mms(number = '+12146051373', MSG= MSG)
     
 # SendEmail.email()
-# SendEmail.recipients = 'jerewalker@teksystems.com', 'mdave@teksystems.com', 'jhagerman@teksystems.com', 'acovarrubio@teksystems.com', 'dchamness@teksystems.com'
+# 'jerewalker@teksystems.com', 'mdave@teksystems.com', 'jhagerman@teksystems.com', 'acovarrubio@teksystems.com', 'dchamness@teksystems.com'
--- send_mms_alert/send_mms.py
+++ send_mms_alert/send_mms.py
@@ -3,7 +3,6 @@ from twilio.rest import Client
 import pyimgur
 
 
-
 class SendText:
     CLIENT_ID = "a779a15bf8a2b82"
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
@@ -11,11 +10,10 @@ class SendText:
     client = Client(account_sid, auth_token)
     url = None
 
-
     @classmethod
     def image_upload(cls, PATH):
         img = pyimgur.Imgur(cls.CLIENT_ID)
-        uploaded_image = img.upload_image(PATH, title="Uploaded with PyImgur")
+        uploaded_image = img.upload_image(PATH, title="FVA Detection")
         print(uploaded_image.title)
         print(uploaded_image.link)
         print(uploaded_image.size)
@@ -23,12 +21,11 @@ class SendText:
         setattr(SendText, 'url', uploaded_image.link)
 
     @classmethod
-    def mms(cls, number):
+    def mms(cls, number, MSG):
         numbers_to_message = [number]
-        # numbers_to_message = numbers_to_message.append(number)
         for number in numbers_to_message:
             message = cls.client.messages.create(
-                body='A detection has made, see attached image!',
+                body= MSG,
                 media_url=[cls.url],
                 from_='+12517664504',
                 to=number
