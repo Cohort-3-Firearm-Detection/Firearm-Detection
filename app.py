5565e48a072fd035dd77cb5c76291dfaa8f9cf6d
james-hagerman <james-hag@hotmail.com>
2022-09-22 18:38:51 -0700

    final integration of smtp email functionality

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from send_mms_alert.send_mms import SendText
-# # from send_email_alert.send_email import SendEmail
 from Tensorflow.live_detect_app import *
-from smtp_alert import SendEmail
+from send_email_alert.smtp_alert import SendEmail
 
 PATH = "Tensorflow/workspace/images/detection.png"
 MSG = "A detection has been made with a confidence level of {}. See the attached image for more information".format(Detection.conf_lev)
@@ -12,14 +11,4 @@ if Detection.conf_lev >= 0.7:
     print('Detection made with a confidence level of {}'.format(Detection.conf_lev))
     SendText.image_upload(PATH)
     SendText.mms(number = '+12146051373', MSG= MSG)
-    
-# SendEmail.email()
-# 'jerewalker@teksystems.com', 'mdave@teksystems.com', 'jhagerman@teksystems.com', 'acovarrubio@teksystems.com', 'dchamness@teksystems.com'
-
-
-from smtp_alert import SendEmail
-
-PATH = "Tensorflow/workspace/images/detection.png"
-MSG = 'this is a test'
-
-SendEmail.send_email(PATH, MSG)
+SendEmail.send_email(PATH, MSG, recipients = ['Dukeofangus@gmail.com'])
