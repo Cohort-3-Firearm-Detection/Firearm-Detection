b9d33459990eb0bac35431db50aca4f5928d76b1
james-hagerman <james-hag@hotmail.com>
2022-09-22 18:16:23 -0700

    adds image attachment for email

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from send_mms_alert.send_mms import SendText
 # # from send_email_alert.send_email import SendEmail
 from Tensorflow.live_detect_app import *
-
+from smtp_alert import SendEmail
 
 PATH = "Tensorflow/workspace/images/detection.png"
 MSG = "A detection has been made with a confidence level of {}. See the attached image for more information".format(Detection.conf_lev)
@@ -15,3 +15,11 @@ if Detection.conf_lev >= 0.7:
     
 # SendEmail.email()
 # 'jerewalker@teksystems.com', 'mdave@teksystems.com', 'jhagerman@teksystems.com', 'acovarrubio@teksystems.com', 'dchamness@teksystems.com'
+
+
+from smtp_alert import SendEmail
+
+PATH = "Tensorflow/workspace/images/detection.png"
+MSG = 'this is a test'
+
+SendEmail.send_email(PATH, MSG)
